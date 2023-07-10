4ba5b0e1ab99a84ce81d6b99a607a6d1b7177f2f
minnnisu <mine52100@naver.com>
2023-07-10 15:46:29 +0900

    modify TestMessageED and add Test4c3Combination

--- example_test.go
+++ example_test.go
@@ -2,6 +2,7 @@ package fountain
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,17 +15,6 @@ import (
 type Data struct {
 	Name        string `json:"name"`
 	Account  	string `json:"account"`
-}
-
-func Encode(message []byte, e int, c Codec, seed int64) []LTBlock{
-	ids := make([]int64, e)
-	random := rand.New(rand.NewSource(seed))
-	for i := range ids {
-		ids[i] = int64(random.Intn(60000))
-	}
-	codeBlocks := EncodeLTBlocks(message, ids, c)
-
-	return codeBlocks
 }
 
 func outputJsonFile(jsonData []Data){
@@ -60,15 +50,68 @@ func compareTwoJson(json1 []Data, json2 []Data){
 	}	
 }
 
+func serializeCodeBlock(codeBlocks []LTBlock) []byte{
+	bytes, err := json.Marshal(codeBlocks)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return bytes
+}
+
+func deserializeCodeBlock(bytes []byte) ([]LTBlock){
+	var newLTBlock []LTBlock
+	err := json.Unmarshal(bytes, &newLTBlock)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return newLTBlock
+}
+
+func encode(message []byte, kSize int, encodedSymbolSize int, seed int64) []byte{
+	c := NewRaptorCodec(kSize, 4)
+	ids := make([]int64, encodedSymbolSize)
+	random := rand.New(rand.NewSource(seed))
+
+	messageCopy := make([]byte, len(message))
+	copy(messageCopy, message)
+
+	for i := range ids {
+		ids[i] = int64(random.Intn(60000))
+	}
+
+	encodedBlocks := EncodeLTBlocks(messageCopy, ids, c)
+	serializedEncodedBlocks := serializeCodeBlock(encodedBlocks)
+	
+	return serializedEncodedBlocks
+}
+
+func decode(serializedEncodedBlocks [][]byte, kSize int, messageSize int) ([]byte, error){
+	c := NewRaptorCodec(kSize, 4)
+	decoder := newRaptorDecoder(c.(*raptorCodec), messageSize)
+
+	for i := 0; i < len(serializedEncodedBlocks); i++ {
+		EncodedBlocks := deserializeCodeBlock(serializedEncodedBlocks[i])
+		for j := 0; j < len(EncodedBlocks); j++ {
+			if (decoder.AddBlocks([]LTBlock{EncodedBlocks[j]})) {
+				out := decoder.Decode()
+				return out, nil
+			}
+		}
+	}
+
+	return nil, errors.New("Not Decoding!")
+}
+
 func TestAvailableKsize(t *testing.T) {
 	availableKsize := make([]int, 0)
-	filePath := "dummy_data/50MB_dummy.json"
+	filePath := "dummy_data/4c3_data/dummy1.json"
 	message, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for kSize := 100; kSize < 110; kSize++ {
+	for kSize := 4; kSize < 100; kSize++ {
 		c := NewRaptorCodec(kSize, 4)
 		ids := make([]int64, kSize + 50)
 		random := rand.New(rand.NewSource(8923489))
@@ -102,78 +145,118 @@ func TestAvailableKsize(t *testing.T) {
 	fmt.Printf("AvailableKsize: %v\n", availableKsize)
 }
 
-func serializeCodeBlockToByte(codeBlocks []LTBlock) []byte{
-	bytes, err := json.Marshal(codeBlocks)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return bytes
-}
-
-func serializeByteToCodeBlock(bytes []byte) ([]LTBlock){
-	var newLTBlock []LTBlock
-	err := json.Unmarshal(bytes, &newLTBlock)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return newLTBlock
-}
-
 func TestMessageED(t *testing.T) {
-	// JSON 파일 열기
 	filePath := "dummy_data/50MB_dummy.json"
 	message, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	c := NewRaptorCodec(45, 4)
+	kSize := 45
+	encodedSymbolSize := 20
 
-	messageCopy1 := make([]byte, len(message))
-	copy(messageCopy1, message)
+	serializedEncodedBlocks := make([][]byte, 3)
 
-	messageCopy2 := make([]byte, len(message))
-	copy(messageCopy2, message)
+	
+	serializedEncodedBlocks[0] = encode(message, kSize, encodedSymbolSize, 8923483)
+	fmt.Println(len(serializedEncodedBlocks[0]))
+	serializedEncodedBlocks[1] = encode(message, kSize, encodedSymbolSize, 8923486)
+	serializedEncodedBlocks[2] = encode(message, kSize, encodedSymbolSize, 8923487)
 
-	codeBlocks1 := Encode(messageCopy1, 100, c, 8923485)
-	codeBlocks2 := Encode(messageCopy2, 100, c, 8923487)
+	out, err := decode(serializedEncodedBlocks, kSize, len(message))
 
-	byteData1 := serializeCodeBlockToByte(codeBlocks1)
-	reCodeBlock1 := serializeByteToCodeBlock(byteData1)
-
-	byteData2 := serializeCodeBlockToByte(codeBlocks2)
-	reCodeBlock2 := serializeByteToCodeBlock(byteData2)
-
-	t.Log("DECODE--------")
-	decoder := newRaptorDecoder(c.(*raptorCodec), len(message))
-
-	i := 0
-	for !decoder.matrix.determined() && i < 70 {
-		fmt.Println(decoder.AddBlocks([]LTBlock{reCodeBlock1[i]}))
-		fmt.Println(decoder.AddBlocks([]LTBlock{reCodeBlock2[i+1]}))
-		i += 2
+	if err != nil {
+		t.Errorf(err.Error())
 	}
+	
+	if !reflect.DeepEqual(message, out) {
+		t.Errorf("Different!")
+	}else{
+		var jsonData []Data
+		err = json.Unmarshal(out, &jsonData)
 
-	if decoder.matrix.determined() {
-		// t.Log("Recovered:\n", decoder.matrix.v)
-		out := decoder.Decode()
-
-		// // JSON 데이터를 Go 데이터 구조로 언마샬링
-		// var recoveredJsonData []Data
-		// err = json.Unmarshal(out, &recoveredJsonData)
-
-		// if err != nil {
-		// 	log.Printf("error decoding sakura response: %v", err)
-		// 	if e, ok := err.(*json.SyntaxError); ok {
-		// 		log.Printf("syntax error at byte offset %d", e.Offset)
-		// 	}
-		// }
-
-		if !reflect.DeepEqual(message, out) {
-			t.Errorf("Not different")
+		if err != nil {
+			t.Errorf(err.Error())
 		}
-	}else {
-		t.Errorf("Not determined")
+		
+		outputJsonFile(jsonData)
 	}
+}
+
+func Test4c3Combination(t *testing.T){
+	json1Path := "dummy_data/4c3_data/dummy1.json"
+	json2Path := "dummy_data/4c3_data/dummy2.json"
+	kSize := 7
+	encodedSymbolSize := 3
+
+
+	message1, err := ioutil.ReadFile(json1Path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	message2, err := ioutil.ReadFile(json2Path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	producer1 := encode(message1, kSize, encodedSymbolSize, 8923483)
+	producer2 := encode(message1, kSize, encodedSymbolSize, 8923484)
+	producer3 := encode(message1, kSize, encodedSymbolSize, 9234855)
+	producer4 := encode(message2, kSize, encodedSymbolSize, 8923486) // malicious
+
+	serializedEncodedBlocks1 := make([][]byte, 3)
+	serializedEncodedBlocks1[0] = producer2
+	serializedEncodedBlocks1[1] = producer3
+	serializedEncodedBlocks1[2] = producer4
+
+	serializedEncodedBlocks2 := make([][]byte, 3)
+	serializedEncodedBlocks2[0] = producer1
+	serializedEncodedBlocks2[1] = producer3
+	serializedEncodedBlocks2[2] = producer4
+
+	serializedEncodedBlocks3 := make([][]byte, 3)
+	serializedEncodedBlocks3[0] = producer1
+	serializedEncodedBlocks3[1] = producer2
+	serializedEncodedBlocks3[2] = producer4
+
+	serializedEncodedBlocks4 := make([][]byte, 3) // Not malicious
+	serializedEncodedBlocks4[0] = producer1
+	serializedEncodedBlocks4[1] = producer2
+	serializedEncodedBlocks4[2] = producer3
+
+	out1, err := decode(serializedEncodedBlocks1, kSize, len(message1))
+	out2, err := decode(serializedEncodedBlocks2, kSize, len(message1))
+	out3, err := decode(serializedEncodedBlocks3, kSize, len(message1))
+	out4, err := decode(serializedEncodedBlocks4, kSize, len(message1))
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	var jsonData []Data
+	err = json.Unmarshal(out1, &jsonData)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Printf("out1: %v\n", string(out1))
+
+	err = json.Unmarshal(out2, &jsonData)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Printf("out2: %v\n", string(out2))
+
+	err = json.Unmarshal(out3, &jsonData)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Printf("out3: %v\n", string(out3))
+
+	err = json.Unmarshal(out4, &jsonData)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Printf("out4: %v\n", string(out4))
+
 }
